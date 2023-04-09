ea25b78eab6e45d7734b6a92f7caf23a48bbff2e
deshandeshandeshan <deshan.mclachlan0@gmail.com>
2023-04-10 11:46:38 +1200

    Homepage component created and added general skeleton of content

---
+++ src/components/Homepage.js
@@ -0,0 +1,40 @@
+return (
+  <main>
+    <h1>Cosmic Energy with Aimee</h1>
+
+    <h2>Unlock your self-empowerment and clear blocks with Aimee.</h2>
+
+    <h2>Welcome to Cosmic Energy</h2>
+    <p>
+      We often hear it nowadays don’t we - how we are all made up of energy, and
+      that is true for everything in our world, so when your own internal energy
+      system (the chakras) are out of sync, it really does have a profound
+      effect on our reality and how we respond to things happening around us. I
+      offer a range remote services that promote natural healing and spiritual
+      growth. Take a look!
+    </p>
+    <button>Services</button>
+
+    <h2>Let's discover whats holding you back</h2>
+    <p>
+      Here at Cosmic Energy, my goal is to help you clear any blockages that are
+      stopping you from aligning with your full potential and your higher self.
+      Allow me to help you on your journey…
+    </p>
+    <b>Enquire</b>
+
+    <h2>Hi, I'm Aimee</h2>
+    <p>
+      I have consciously been on my own spiritual journey since I was 17 years
+      of age. Fast forward a few years and some losses later, I finally chose to
+      answer the “call” to unlock who I really am after losing my Paternal
+      grandmother back in 2008. I am passionate about being a catalyst for my
+      clients to unlock their self-empowerment and clear blocks that are
+      stopping their soul connection on their journey and who they want to be. I
+      see you, I hear you, I feel you.
+    </p>
+    <button>About me</button>
+  </main>
+);
+
+export default Homepage;
