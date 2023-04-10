7609454bb6076f978440b6ea0d5f532c33fbb650
deshandeshandeshan <deshan.mclachlan0@gmail.com>
2023-04-10 12:08:28 +1200

    Fixed Header and Homepage component error for return sitting outside of function

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,13 @@
-return (
-  <main>
-    <h1>Cosmic Energy with Aimee</h1>
-    <p>Homepage</p>
-    <p>Services</p>
-    <p>Enqire</p>
-    <p>About me</p>
-  </main>
-);
+const Header = () => {
+  return (
+    <main>
+      <h1>Cosmic Energy with Aimee</h1>
+      <p>Homepage</p>
+      <p>Services</p>
+      <p>Enqire</p>
+      <p>About me</p>
+    </main>
+  );
+};
 
 export default Header;
