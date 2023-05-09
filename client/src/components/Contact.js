51fc73eb98cf7b03c17fbde19b849aeb76bea1ed
deshandeshandeshan <deshan.mclachlan0@gmail.com>
2023-05-09 13:19:30 +1200

    Content / form added to contact page

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,7 +1,27 @@
 const Enquire = () => {
   return (
     <main>
-      <h1>Enquire</h1>
+      <div>
+        <h1>Enquire</h1>
+        <p>Enquire here for a service and I’ll get in contact</p>
+        <form>
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name" />
+
+          <label for="email">Email:</label>
+          <input type="text" id="email" name="email" />
+
+          <label for="service">Service:</label>
+          <select name="services" id="service">
+            <option value="reiki">Reiki</option>
+            <option value="oracle">Oracle card reading</option>
+            <option value="medium">Mediumship reading</option>
+          </select>
+
+          <label for="message">Message:</label>
+          <input type="text" id="message" name="message" />
+        </form>
+      </div>
     </main>
   );
 };
