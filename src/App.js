71079fb6889faa2e7908c65e8fa78b1031713398
deshandeshandeshan <deshan.mclachlan0@gmail.com>
2023-06-16 12:08:16 +1200

    Fixed an error with Home qol fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./components/Homepage";
+import Home from "./components/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Services from "./components/Services";
