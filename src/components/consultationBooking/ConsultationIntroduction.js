d83d3bb00f7b439786ef06762ab4b5b2b68a1094
deshandeshandeshan <deshan.mclachlan0@gmail.com>
2023-06-23 14:41:10 +1200

    Took away styling from consultation introduction component

---
+++ src/components/consultationBooking/ConsultationIntroduction.js
@@ -0,0 +1,12 @@
+import { InlineWidget } from "react-calendly";
+import "./ConsultationBooking.css";
+
+const consultationIntroduction = () => {
+  return (
+    <main className="consultationPage">
+      <InlineWidget url="https://calendly.com/hello-45w/introductory-call?background_color=e8e3d6&text_color=20123e&primary_color=8a26a0" />
+    </main>
+  );
+};
+
+export default consultationIntroduction;
