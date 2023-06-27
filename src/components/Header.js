32a9253c7a66481146bdfc2c9a13d3c894627c28
deshandeshandeshan <deshan.mclachlan0@gmail.com>
2023-06-27 14:51:05 +1200

    Implimented usestate and useeffect in header component to achieve hiding the nav on scroll

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,35 @@
 import { Link } from "react-router-dom";
 import Logo from "../images/Logo.png";
 import "./Header.css";
+import React, { useState, useEffect } from "react";
 
 const Header = () => {
+  const [showNav, setShowNav] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlNavBar = () => {
+    if (window.scrollY < lastScrollY) {
+      setShowNav(false);
+    } else {
+      setShowNav(true);
+    }
+
+    setLastScrollY(window.scrollY);
+
+    console.log(showNav);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavBar);
+    return () => {
+      window.removeEventListener("scroll", controlNavBar);
+    };
+  });
+
   return (
     <div>
       <header>
-        <nav className="navBar">
+        <nav className={`navBar ${showNav && "nav--hidden"}`}>
           <Link to="/" className="navLink">
             <img
               src={Logo}
