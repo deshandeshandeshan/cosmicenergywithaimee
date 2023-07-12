a17400da73826adee1d510669bf073aa28cef7d2
deshandeshandeshan <deshan.mclachlan0@gmail.com>
2023-07-12 12:02:37 +1200

    Made adjustments to consultation booking pages to make them better suited for smaller screens.

--- src/components/consultationBooking/ConsultationIntroduction.js
+++ src/components/consultationBooking/ConsultationIntroduction.js
@@ -3,7 +3,7 @@ import "./ConsultationBooking.css";
 
 const consultationIntroduction = () => {
   return (
-    <main className="consultationPage">
+    <main className="consultationPage consultationPadding">
       <InlineWidget url="https://calendly.com/hello-45w/introductory-call?background_color=e8e3d6&text_color=20123e&primary_color=8a26a0" />
     </main>
   );
--- src/components/consultationBooking/ConsultationPackage.js
+++ src/components/consultationBooking/ConsultationPackage.js
@@ -3,7 +3,7 @@ import "./ConsultationBooking.css";
 
 const consultationPackage = () => {
   return (
-    <main className="consultationPage">
+    <main className="consultationPage consultationPadding">
       <InlineWidget url="https://calendly.com/hello-45w/60-minute-meeting?background_color=e8e3d6&text_color=20123e&primary_color=8a26a0" />
     </main>
   );
