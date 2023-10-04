558706262e1d09e0b31c2f165a4a220f6e66f2c6
Deshan Mclachlan <deshan.mclachlan0@gmail.com>
2023-10-04 18:39:28 +1300

    Updated services to reflect Aimees changes. Changed the styling for the consult booking page

--- src/components/consultationBooking/ConsultationIntroduction.js
+++ src/components/consultationBooking/ConsultationIntroduction.js
@@ -4,7 +4,9 @@ import "./ConsultationBooking.css";
 const consultationIntroduction = () => {
   return (
     <main className="consultationPage consultationPadding">
-      <InlineWidget url="https://calendly.com/hello-45w/introductory-call?background_color=e8e3d6&text_color=20123e&primary_color=8a26a0" />
+      <div>
+        <InlineWidget url="https://calendly.com/hello-45w/introductory-call?background_color=e8e3d6&text_color=20123e&primary_color=8a26a0" />
+      </div>
     </main>
   );
 };
